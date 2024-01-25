31dce7e001d46e041ad179c86d8978bc043c39dc
Simone Matsukis <simonem@tecex.com>
2024-01-25 19:23:26 +0200

    fix after renaming jpgs

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
 
   return (
     <>
-    <div className="relative bg-cover bg-center h-screen font-minimal transition ease-in duration-1000" style={{ backgroundImage: 'url(src/assets/S+C-023.jpg)' }}>
+    <div className="relative bg-cover bg-center h-screen font-minimal transition ease-in duration-1000" style={{ backgroundImage: 'url(src/assets/home.jpg)' }}>
       <div className="absolute bottom-0 left-0 right-0 p-8 text-zinc-800">
         <h1 className="font-bold mb-4 font-cursive text-6xl">Simone & Carl</h1>
         <p className="text-2xl">17 August 2024 - The Nut Farm</p>
