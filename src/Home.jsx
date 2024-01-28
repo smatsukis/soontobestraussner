90df654b678e1d51cbef9f236d3737d3f4d53674
Simone Matsukis <simonem@tecex.com>
2024-01-28 19:23:22 +0200

    changes for vite

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import NavLinkCustom from './components/NavLinkCustom';
 import CountdownTimer from './components/Countdown';
 import NavBar from './components/NavBar';
+import homeImage from './assets/home.jpg';
 
 function Home() {
   const [isVisible, setIsVisible] = useState(false);
@@ -13,7 +14,7 @@ function Home() {
   return (
     <div className={`transition-opacity duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
       <div className="flex flex-col h-screen">
-        <div className="relative flex-1 bg-cover bg-center font-minimal" style={{ backgroundImage: 'url(src/assets/home.jpg)' }}>
+        <div className="relative flex-1 bg-cover bg-center font-minimal" style={{ backgroundImage: `url(${homeImage})` }}>
           <NavBar/>
           <div className="absolute bottom-0 left-0 right-0 p-8 text-zinc-800">
             <h1 className="font-bold mb-4 font-cursive text-6xl">Simone & Carl</h1>
